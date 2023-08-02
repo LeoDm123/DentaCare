93a452537a8f4fd076d2346263f9fdec1666dfb9
LeoDm123 <leo.meiners1@gmail.com>
2023-08-01 21:51:15 -0300

    codigo js para filtrar doctores segun dia, horario y servicios

--- js/doctorFilter.js
+++ js/doctorFilter.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const diaSelect = document.getElementById("fecha"); // Corrected ID name
   const horariosSelect = document.getElementById("horarios");
   const serviciosSelect = document.getElementById("servicios");
   const doctoresSelect = document.getElementById("doctores");
@@ -8,46 +9,50 @@ document.addEventListener("DOMContentLoaded", function () {
       nombre: "Luis Figueroa",
       horarios: [1, 2, 3, 7, 8, 9, 10, 11],
       servicios: [1, 2, 3, 6],
+      diasTrabajo: [0, 1, 2, 3, 4],
     },
     {
       nombre: "Aaron Huetagoyena",
       horarios: [1, 2, 3, 4, 5],
       servicios: [4, 5, 6, 7],
+      diasTrabajo: [0, 2, 4, 5],
     },
     {
       nombre: "Valentina Tincani",
       horarios: [6, 7, 8, 9, 10, 11],
       servicios: [1, 2, 3, 4, 5, 6],
+      diasTrabajo: [1, 3, 5],
     },
     {
       nombre: "Francisco Giuoptionano",
       horarios: [1, 2, 3, 4, 5],
       servicios: [1, 2, 4, 5, 6],
+      diasTrabajo: [0, 1, 2, 3, 4],
     },
     {
       nombre: "Leandro Meiners",
       horarios: [1, 2, 3, 7, 8, 9, 10, 11],
       servicios: [1, 3, 6, 7, 8],
+      diasTrabajo: [0, 1, 2, 4, 5],
     },
-    // ... Add more doctors with their horarios and servicios
   ];
 
   function updateDoctorList() {
-    const selectedHorario = horariosSelect.value;
-    const selectedServicio = serviciosSelect.value;
+    const selectedDia = new Date(diaSelect.value).getDay(); // Get day of the week (0-6)
+    const selectedHorario = parseInt(horariosSelect.value);
+    const selectedServicio = parseInt(serviciosSelect.value);
+
+    console.log(selectedDia, selectedHorario, selectedServicio);
 
     const filteredDoctores = doctores.filter(
       (doctor) =>
-        doctor.horarios.includes(parseInt(selectedHorario)) &&
-        doctor.servicios.includes(parseInt(selectedServicio))
+        doctor.diasTrabajo.includes(selectedDia) &&
+        doctor.horarios.includes(selectedHorario) &&
+        doctor.servicios.includes(selectedServicio)
     );
 
-    // Clear the current options
-    while (doctoresSelect.firstChild) {
-      doctoresSelect.removeChild(doctoresSelect.firstChild);
-    }
+    doctoresSelect.innerHTML = "";
 
-    // Add filtered doctors to the select options
     filteredDoctores.forEach((doctor) => {
       const option = document.createElement("option");
       option.value = doctor.nombre;
@@ -56,6 +61,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
+  diaSelect.addEventListener("change", updateDoctorList);
   horariosSelect.addEventListener("change", updateDoctorList);
   serviciosSelect.addEventListener("change", updateDoctorList);
+
+  updateDoctorList();
 });
